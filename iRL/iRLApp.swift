5aab325fa392c717791b927c2ecd217c98cb411e
delinak <delina.k14@gmail.com>
2025-07-31 17:48:45 -0400

    Initial Commit

---
+++ iRL/iRLApp.swift
@@ -0,0 +1,17 @@
+//
+//  iRLApp.swift
+//  iRL
+//
+//  Created by delina on 7/31/25.
+//
+
+import SwiftUI
+
+@main
+struct iRLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
